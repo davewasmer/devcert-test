cb1c6cc22eec446a7b5a5c448a456042f45a0039
Dave Wasmer <davewasmer@gmail.com>
2018-01-06 21:03:36 -0700

    add args support

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const devcert = require('devcert');
 const https = require('https');
 
-devcert.certificateFor('my-app.test').then((ssl) => {
+let skipCertutilInstall = process.argv.find((arg) => arg === '--skip-certutil-install')
+let skipHostsFile = process.argv.find((arg) => arg === '--skip-hosts-file')
+
+devcert.certificateFor('my-app.test', { skipCertutilInstall, skipHostsFile }).then((ssl) => {
   https.createServer(ssl, (req, res) => {
     res.write('<h1>Hello world - devcert is working</h1>');
     res.end();
